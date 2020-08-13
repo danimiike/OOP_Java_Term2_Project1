643f6b73c77ef6fd469a0d74e794cfd56c5d23ec
Danielle <danimiike@gmail.com>
2020-08-13 15:56:58 -0400

    Create InterestPayable.java

---
+++ InterestPayable.java
@@ -0,0 +1,11 @@
+/**
+	 * Interface Class Name:	InterestPayable
+	 * Purpose:					This class will be implemented by the both classes, PersonalChequingAccout or SavingsAccount.
+	 * Coder:					Danielle Miike
+	 * Date:					Fev 16, 2020
+*/
+
+public interface InterestPayable {
+
+	abstract void calcInterest();
+}
